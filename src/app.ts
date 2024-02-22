8e52d97a640eeca6be09fec51b2c446551c1ebe8
Manuel Fernando Ramirez <manuel.ramirez1209@gmail.com>
2024-02-22 05:27:30 -0500

    create db structure

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,21 @@
 import 'dotenv/config';
 import { Server } from "./presentation/server";
 import { envs } from './config/plugins/envs.plugin';
+import { MongoDatabase } from './data/mongo';
+import { env } from 'process';
 
 
 (async() => {
     main( )
 })();
 
-function main(){
+async function main(){
+
+    await MongoDatabase.connect({
+        mongoUrl: envs.MONGO_URL,
+        dbName: envs.MONGO_DB_NAME
+    });
+
     Server.start();
     //console.log(envs.PORT);
 }
